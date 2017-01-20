3de442de60ce6f2ea44115464ee7f68d0d0acb61
Salvo Scala <salvoscala@gmail.com>
2017-01-20 17:01:50 +0100

    Update  Roomify travel theme header

--- themes/roomify/roomify_travel/color/preview.php
+++ themes/roomify/roomify_travel/color/preview.php
@@ -1,7 +1,12 @@
 <div id="preview">
   <!-- Header. -->  
   <header id="header">
-
+    <span class="main-menu">
+      <a class="roomify-main-menu-hamburger" href="#menu-toggle">
+        <!-- PRINT SVG USER ICON -->
+        <svg height="32px" id="Layer_1" style="enable-background:new 0 0 32 32;" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z"/></svg>
+      </a>
+    </span>
     <div class="logo">
       <img class="img-responsive" src="<?php
         $theme = variable_get('theme_default', 'bartik');
@@ -12,22 +17,6 @@
           print file_create_url(theme_get_setting('logo_path', $theme));
         }
       ?>">
-    </div>
-    <div class="main-menu">
-      <ul class="menu nav navbar-nav">
-        <li>
-          <a href="/">Activities </a>
-        </li>
-        <li>
-          <a href="/">Blog</a>
-        </li>
-        <li>
-          <a href="/">Location</a>
-        </li>
-        <li>
-          <a href="/">Contact Us</a>
-        </li>
-      </ul>
     </div>
     <span class="roomify-sidebar-link">
       <a class="roomify-sidebar-menu-toggle" href="#menu-toggle">
