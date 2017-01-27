c5eb0d6cd402dbbd7ddd6cc520867626bafab74e
Nicolò Caruso <nicolo@istos.it>
2017-01-27 18:06:06 +0100

    'View Enquiry Details' and ‘Hide Enquiry Details'

--- modules/roomify/roomify_conversations/js/roomify_conversations.js
+++ modules/roomify/roomify_conversations/js/roomify_conversations.js
@@ -4,7 +4,20 @@ Drupal.behaviors.roomifyConversations = {
   attach: function(context) {
 
     $('.expand-enquiry').click(function() {
-      $('.enquiry').toggle();
+      $('.enquiry').show();
+
+      $('.expand-enquiry').parent().hide();
+      $('.hide-enquiry').parent().show();
+
+      return false;
+    });
+
+    $('.hide-enquiry').click(function() {
+      $('.enquiry').hide();
+
+      $('.expand-enquiry').parent().show();
+      $('.hide-enquiry').parent().hide();
+
       return false;
     });
 
