85e9047438614310f13398029b4501d44a18502b
salvoscala <salvoscala@gmail.com>
2017-03-28 18:19:31 +0200

    Add the Guest Reviews feature.

--- themes/roomify/roomify_travel/assets/scripts/roomify_travel.js
+++ themes/roomify/roomify_travel/assets/scripts/roomify_travel.js
@@ -73,4 +73,27 @@ Drupal.behaviors.roomifyTravelScripts = {
   }
 };
 
+
+Drupal.behaviors.roomifyTravelReadMoreElements = {
+  attach: function (context, settings) {
+
+    $('.page-listing .view-reviews .comment').readmore({
+      speed: 75,
+      collapsedHeight: 50,
+      moreLink: '<a class="collapse-element-link" href="#">' + Drupal.t('Read more') + '</a>',
+      lessLink: '<a class="collapse-element-link" href="#">' + Drupal.t('Read less') + '</a>',
+    });
+    // If the user has another tab open as default we should recreate the readmore.
+    tabParent = $('#property-reviews-tab').parent();
+    $(tabParent).click(function() {
+      $('.page-listing .view-reviews .comment').readmore({
+        speed: 75,
+        collapsedHeight: 50,
+        moreLink: '<a class="collapse-element-link" href="#">' + Drupal.t('Read more') + '</a>',
+        lessLink: '<a class="collapse-element-link" href="#">' + Drupal.t('Read less') + '</a>',
+      });
+    });
+  }
+}
+
 })(jQuery);
