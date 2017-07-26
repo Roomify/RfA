77db39d25ca3544913eba65679be0f6b72176710
nicolocaruso85 <nicolo@istos.it>
2017-07-26 09:57:20 +0200

    Create a new content type and let managers create pages with lists of properties. (#245)
    * Integrate Roomify Properties list
    
    * Enable module "Roomify Properties List"
    
    * Filter properties from content "Roomify Properties List"
    
    * Code style
    
    * Improve page.
    
    * Improve page
    
    * Add highlights and a new layout
    
    * Change file name.
    
    * Chenge function name
    
    * Add 3 nodes and improve page.
    
    * Integrate layout
    
    * Integration with base themes.
    
    * Code Style

--- themes/roomify/roomify_travel/layouts/bootstrap_properties_list/bootstrap-properties-list.tpl.php
+++ themes/roomify/roomify_travel/layouts/bootstrap_properties_list/bootstrap-properties-list.tpl.php
@@ -4,6 +4,11 @@
   </div>
   <div class="panel-content-wrapper">
     <div class="panel_sl_content col-sm-12"><?php print $content['middle'];?></div>
+    <div class="highlights">
+      <div class="panel_sl_highlight1 col-xs-12 col-sm-4"><div class="inner"><?php print $content['highlight1'];?></div></div>
+      <div class="panel_sl_highlight2 col-xs-12 col-sm-4"><div class="inner"><?php print $content['highlight2'];?></div></div>
+      <div class="panel_sl_highlight3 col-xs-12 col-sm-4"><div class="inner"><?php print $content['highlight3'];?></div></div>
+    </div>
     <div class="panel_sl_content-2 col-sm-12"><div class="inner"><?php print $content['middle-second'];?></div></div>
   </div>
   <div class="panel-content-bottom">
